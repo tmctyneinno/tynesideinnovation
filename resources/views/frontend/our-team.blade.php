9adb4c5f988ff76ea4981048d45420301a18d84e
Michael Ozoudeh <jesmikky@gmail.com>
2025-03-02 00:10:03 +0100

    completed team members page

--- resources/views/frontend/our-team.blade.php
+++ resources/views/frontend/our-team.blade.php
@@ -42,7 +42,7 @@
                         <span class="section-title-two__tagline">Creative Team</span>
                     </div>
                     <h2 class="section-title-two__title title-animation">Connect with our
-                        <span>skilled team</span><br>and tap into their expertise.</h2>
+                        <span>skilled team</span></h2>
                 </div>
                 <div class="row">
                     <!--Team Page Single Start-->
@@ -50,27 +50,170 @@
                         <div class="team-page__single">
                             <div class="team-page__img-box">
                                 <div class="team-page__img">
-                                    <img src="assets/images/team/team-page-1-1.jpg" alt="">
+                                    <img src="{{asset('/assets/images/team/mike.png')}}" alt="">
                                 </div>
                             </div>
                             <div class="team-page__content">
-                                <h4 class="team-page__title"><a href="#"></a></h4>
-                                <p class="team-page__text"></p>
+                                <h4 class="team-page__title"><a href="#">Michael Ozoudeh</a></h4>
+                                <p class="team-page__text">Team Lead: Software Department</p>
                                 <div class="team-page__social">
-                                    <div class="team-page__social-shape-1">
-                                        <img src="assets/images/shapes/team-page-social-shape-1.png" alt="">
-                                    </div>
                                     <div class="team-page__social-shape-2">
-                                        <img src="assets/images/shapes/team-page-social-shape-2.png" alt="">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
                                     </div>
-                                    <a href="#"><span class="icon-pinterest"></span></a>
                                     <a href="#"><span class="icon-linkedin"></span></a>
-                                    <a href="#"><span class="icon-twitter"></span></a>
-                                    <a href="#"><span class="icon-facebook"></span></a>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/emma.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Emmanuel Amusa</a></h4>
+                                <p class="team-page__text">Digital Marketing Specialist</p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/deji.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Oladeji Adeloye</a></h4>
+                                <p class="team-page__text">Digital Marketing Specialist</p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/mat.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Matthew T. Adeosun</a></h4>
+                                <p class="team-page__text">Supervisor, Designs and Production.
+                                    </p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/dan.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Daniel Eshanokpe</a></h4>
+                                <p class="team-page__text">Fullstack Developer</p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/jos.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Joseph Iwuji</a></h4>
+                                <p class="team-page__text">Fullstack Developer</p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/taj.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Bello Tajudeen</a></h4>
+                                <p class="team-page__text">Business Development Executive</p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
+                     <!--Team Page Single Start-->
+                     <div class="col-xl-3 col-lg-6 col-md-6">
+                        <div class="team-page__single">
+                            <div class="team-page__img-box">
+                                <div class="team-page__img">
+                                    <img src="{{asset('/assets/images/team/alex.png')}}" alt="">
+                                </div>
+                            </div>
+                            <div class="team-page__content">
+                                <h4 class="team-page__title"><a href="#">Alex Duru</a></h4>
+                                <p class="team-page__text">Business Development Executive</p>
+                                <div class="team-page__social">
+                                    <div class="team-page__social-shape-2">
+                                        {{-- <img src="assets/images/shapes/team-page-social-shape-2.png" alt=""> --}}
+                                    </div>
+                                    <a href="#"><span class="icon-linkedin"></span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {{-- ------end---- --}}
                   
                 </div>
             </div>
