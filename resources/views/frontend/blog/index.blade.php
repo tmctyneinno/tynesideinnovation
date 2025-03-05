2466c2ffd57f392286b88e6a1cf0779ba2ffaa2a
Michael Ozoudeh <jesmikky@gmail.com>
2025-03-05 09:55:55 +0100

    change blog

--- resources/views/frontend/blog/index.blade.php
+++ resources/views/frontend/blog/index.blade.php
@@ -32,7 +32,7 @@
                     @forelse ($blogs as $blog)
                     <div class="col-xl-4 col-lg-6 col-md-6">
                         <div class="blog-two__single">
-                            <div class="blog-two__img">
+                            <div class="blog-two__img" style="width: 200px">
                                 <img src="assets/images/blog/blog-2-1.jpg" alt="">
                                 <div class="blog-two__plus">
                                     <a href="{{route('blog.details', $blog->slug)}}"><span class="icon-plus"></span></a>
@@ -41,7 +41,7 @@
                             <div class="blog-two__content">
                                 <div class="blog-two__content-inner">
                                     <h3 class="blog-two__title"><a href="{{route('blog.details', $blog->slug)}}">{{ $blog->title}}</a></h3>
-                                    <p class="blog-two__text">{!! Str::limit($blog->content, 30) !!} </p>
+                                    {{-- <p class="blog-two__text">{!! Str::limit($blog->content, 30) !!} </p> --}}
                                 </div>
                                 <div class="blog-two__read-more">
                                     <div class="blog-two__read-more-line"></div>
